baf74656b9ee6960406f4e08cca4ca7ea91ed019
sparshSL <34803933+sparshSL@users.noreply.github.com>
2021-03-30 16:44:57 +0530

    Add project files.

---
+++ Scripts/EmployeeProcessor.js
@@ -0,0 +1,7 @@
+define(["require", "exports", "./Employee"], function (require, exports, Employee_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    let obj = new Employee_1.Employee("Sparsh S.L", 12321);
+    obj.displayEmployee();
+});
+//# sourceMappingURL=EmployeeProcessor.js.map
---
+++ Scripts/EmployeeProcessor.ts
@@ -0,0 +1,3 @@
+﻿import { Employee as Associate } from "./Employee"
+let obj = new Associate("Sparsh S.L", 12321);
+obj.displayEmployee();//Output: Employee Code: 3, Employee Name: James Bond
